a205fc878145fed9a4df0a3563775875bfde1d0b
mjc715 <mjc715@miami.edu>
2022-10-05 15:48:15 -0400

    1st method wasnt working, trying to use jquery now

--- currentDJs.js
+++ currentDJs.js
@@ -1,50 +1,34 @@
 var fs = require('fs');
-var parse = require('csv-parse');
+const { parse } = require('csv-parse');
+
+
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+document.getElementsByTagName('head')[0].appendChild(script);
 
 var refreshMins = 5;
 var msPerMin = 1000*60;
 
 function readCSV() {
-    let entries = [];
-    let count = 0;
-
-    fs.createReadStream('Schedule.csv')
-        .pipe(parse({delimiter: ',', from_line: 2}))
-        .on('data', function(row) {
-            count++;
-            entries.push(new Show(row[0], row[1],row[2],row[3],row[4],row[5],row[6]))
-
-            if (count % 10 == 0) {
-                processEntries(entries);
-                count = 0;
-                entries = [];
-            }
-        })
-        .on('end', function () {
-            processEntries(entries);
-        });
-
-}
-
-function processEntries(entries) {
-    console.log(entries[0].Id + "  to " + entries[entries.length - 1].Id);
+    
 }
 
 class Show {
-    constructor(showName, weekday, start, end, DJs, biweeklyEven, biweeklyOdd )
-    ShowName = showName;
-    Weekday = weekday;
-    Start = start;
-    End = end;
-    DJS = DJs;
-    BiweeklyEven = biweeklyEven;
-    BiweeklyOdd = biweeklyOdd;
+    constructor(showName, weekday, start, end, DJs, biweeklyEven, biweeklyOdd) {
+    this.ShowName = showName;
+    this.Weekday = weekday;
+    this.Start = start;
+    this.End = end;
+    this.DJS = DJs;
+    this.BiweeklyEven = biweeklyEven;
+    this.BiweeklyOdd = biweeklyOdd;
+    }
+    
 
 
 }
-
 readCSV();
 for (i = 0; i < entries.length; ++i) {
-    print(entries[i].ShowName)
+    console.log(entries[i].ShowName);
 }
 
