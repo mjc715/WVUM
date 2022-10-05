d837a14606463278290b7723e8f4558c32509fbb
mjc715 <mjc715@miami.edu>
2022-10-05 16:18:39 -0400

    Found csv reading method that works

--- currentDJs.js
+++ currentDJs.js
@@ -1,34 +1,19 @@
-var fs = require('fs');
-const { parse } = require('csv-parse');
+const csv = require('csv-parser');
+const fs = require('fs');
+const DJShows = [];
 
+fs.createReadStream('Schedule.csv')
+    .pipe(csv({}))
+    .on('data', (data) => DJShows.push(data))
+    .on('end', () => {
+        console.log(DJShows);
+    });
 
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
-document.getElementsByTagName('head')[0].appendChild(script);
 
 var refreshMins = 5;
 var msPerMin = 1000*60;
 
-function readCSV() {
-    
-}
-
-class Show {
-    constructor(showName, weekday, start, end, DJs, biweeklyEven, biweeklyOdd) {
-    this.ShowName = showName;
-    this.Weekday = weekday;
-    this.Start = start;
-    this.End = end;
-    this.DJS = DJs;
-    this.BiweeklyEven = biweeklyEven;
-    this.BiweeklyOdd = biweeklyOdd;
-    }
-    
 
 
-}
-readCSV();
-for (i = 0; i < entries.length; ++i) {
-    console.log(entries[i].ShowName);
-}
+
 
