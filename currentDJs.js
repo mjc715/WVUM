15991c445eac23d4700c81cbb421028fa0f60778
mjc715 <mjc715@miami.edu>
2022-10-12 13:16:03 -0400

    added script.js

--- currentDJs.js
+++ currentDJs.js
@@ -1,47 +1,38 @@
-const csv = require('csv-parser');
-const fs = require('fs');
-const DJShows = [];
-
-fs.createReadStream('Schedule.csv')
-    .pipe(csv({}))
-    .on('data', (data) => DJShows.push(data))
-    .on('end', () => {
-        console.log(DJShows);
-    });
-
 var refreshMins = 5;
 var msPerMin = 1000*60;
+
+console.log(DJShows[1].get(ShowName));
 
 function getDJ() {
     var now = new Date();
     for (i = 0; i < DJShows.length; ++i) {
         startSplit = DJShows[i].Start.split(":")
         endSplit = DJShows[i].End.split(":")
-        if (now.getDay == DJShows[i].Weekday && (now.getHours >= parseInt(startSplit[0]) && now.getMinutes >= parseInt(startSplit[1]))
+
     }
 
 }
 
-// var xmlhttp = new XMLHttpRequest();
-// xmlhttp.onreadystatechange = function () {
-//   if (this.readyState == 4 && this.status == 200) {
-//     var myObj = JSON.parse(this.responseText);
-//     document.getElementById("demo").innerHTML = myObj.currentDJ;
-//   }
-// };
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("demo").innerHTML = myObj.currentDJ;
+  }
+};
 
-// function callApi() {
-//   xmlhttp.open("GET", "https://us-central1-wvum-d6fb8.cloudfunctions.net/getDJ", true);
-//   xmlhttp.send();
-// }
+function callApi() {
+  xmlhttp.open("GET", "https://us-central1-wvum-d6fb8.cloudfunctions.net/getDJ", true);
+  xmlhttp.send();
+}
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   console.log("State Ready")
-//   window.setInterval(function () {
-//     callApi();
-//   }, refreshMins * msPerMin);
-//   callApi();
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("State Ready")
+  window.setInterval(function () {
+    callApi();
+  }, refreshMins * msPerMin);
+  callApi();
+});
 
 
 
---
+++ script.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+
+    console.log("ready")
+  
+  
+  //showing current DJ
+  
+    $.getJSON("https://us-central1-wvum-d6fb8.cloudfunctions.net/getDJ", function(result){
+      $("#currentDJ").html(result.currentDJ);
+    });
+    
+  
+  //creating the menu with the library
+    var select = new SlimSelect({
+      select: '#single',
+      placeholder: 'Select show'
+    });
+  
+    var obj = { "currentDJ":"" };
+  
+  //sending the selection after clicking submit
+    $("#submit").click(function(){
+      if (select.selected() === ""){
+        alert("Please select the current show.")
+      }
+      else {
+        obj.currentDJ = select.selected();
+  
+        $.post("https://us-central1-wvum-d6fb8.cloudfunctions.net/setDJ", obj, function(){
+          $.getJSON("https://us-central1-wvum-d6fb8.cloudfunctions.net/getDJ", function(result){
+            $("#currentDJ").html(result.currentDJ)
+          });
+        })
+          .done(function(){
+            console.log("success")
+            var snack = document.getElementById("snackbar")
+            snack.className = "show"
+            setTimeout(function(){
+              snack.className = snack.className.replace("show", "")
+            }, 3000);
+          })
+          .fail(function(){
+            var snack = document.getElementById("failbar")
+            snack.className = "show"
+            setTimeout(function(){
+              snack.className = snack.className.replace("show", "")
+            }, 3000);
+          });
+        
+  
+  
+        
+      };
+    });
+  
+  //warning before closing (almost all browsers have removed support for a custom message)
+    window.onbeforeunload = function() {
+      return "DON'T CLOSE THIS PAGE!"
+  }
+  
+  });
+  
